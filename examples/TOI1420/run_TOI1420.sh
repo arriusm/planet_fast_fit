582ced3d6fd2d7c37c871fa929b9dff1971076c9
arriusm <arri@usm.lmu.de>
2025-05-19 15:36:27 +0200

    V250519

--- examples/TOI1420/run_TOI1420.sh
+++ examples/TOI1420/run_TOI1420.sh
@@ -23,13 +23,14 @@
 # Depth [ppm]     = 13960 +/- 140
 # Duration [h]    = 3.372 +/- 0.15
 
-# manual WST:
-# python ../../planet_fast_fit.py  -save TOI1420_i_WST_43cm.tab.pdf  -tit "TOI 1420:" -P 6.96  -t0 9837.758   -rp 0.12  -b 0.41   -Rs 0.940037  -a 16.5   -off 0.  -rhoplan 1  -u1 0.5  -u2 0.  -F0 1.01  -zoom 3  -alpha 0.15  TOI1420_i_WST_43cm.tab
+python ../../planet_fast_fit.py  -save TOI1420_planet_nofit.pdf  -tit "TOI 1420 (Exoplanet_Archive):"  -zoom 15  -alpha 0.15  TOI1420_TIC0321857016_SEC56.fits
+ 
+# Exoplanet Archive
+# python ../../planet_fast_fit.py  -save TOI1420_Exoplanet_Archive.pdf  -tit "TOI 1420 (Exoplanet_Archive):" -P 6.9561063  -t0 9517.43305   -rp 0.11816  -b 0.412   -Rs 0.940037  -Ms 0.935  -Mp 25.1  -off 0.  -u1 0.5  -u2 0.  -e 0.  -w 90  -zoom 15  -alpha 0.15  TOI1420_TIC0321857016_SEC56.fits
+# 
+python ../../planet_fast_fit.py  -save TOI1420_planet_fast_fit.pdf  -tit "TOI 1420 (Exoplanet_Archive):" -P 6.9561063  -t0 9517.43305   -rp 0.11816  -b 0.5   -Rs 0.940037  -Ms 0.935  -rhop 1.0  -off 0.  -u1 0.5  -u2 0.  -e 0.  -w 90  -zoom 15  -alpha 0.15  TOI1420_TIC0321857016_SEC56.fits
+ 
+# WST
+python ../../planet_fast_fit.py  -save TOI1420_i_WST_43cm.tab_nofit.pdf  -tit "TOI 1420:"  -zoom 15 -alpha 0.5  TOI1420_i_WST_43cm.tab
 
-# manual TESS:
-# python ../../planet_fast_fit.py  -save TOI1420_TIC0321857016_SEC56.fits.pdf  -prec 2001  -tit "TOI 1420:" -P 6.96  -t0 9837.416   -rp 0.12  -b 0.41   -Rs 0.940037  -a 16.5   -off 0.    -u1 0.5  -u2 0.   -zoom 3  -alpha 0.15  TOI1420_TIC0321857016_SEC56.fits
-
-# cd TOI1420_LC
-# for i in *.fits; do python ../../../planet_fast_fit.py  -save ${i}.pdf  -tit "TOI 1420:" -P 6.9561063  -t0 9517.43305   -rp 0.11816  -b 0.412   -Rs 0.940037  -Ms 0.935  -Mp 25.1  -off 0.  -u1 0.5  -u2 0.  -e 0.17  -w 90  -y0 0.97  -y1 1.02  -zoom 3  -alpha 0.15  ${i}; done
-
-python ../../planet_fast_fit.py  -save TOI1420_planet_fast_fit.pdf  -tit "TOI 1420:" -P 6.9561063  -t0 9517.43305   -rp 0.11816  -b 0.412   -Rs 0.940037  -Ms 0.935  -Mp 25.1  -off 0.  -u1 0.5  -u2 0.  -e 0.17  -w 90  -zoom 3  -alpha 0.15  TOI1420_TIC0321857016_SEC56.fits
+python ../../planet_fast_fit.py  -save TOI1420_i_WST_43cm.tab_planet_fast_fit.pdf  -tit "TOI 1420:"  -P 6.9561063  -t0 9517.43305   -rp 0.11816  -b 0.5   -Rs 0.940037  -Ms 0.935  -rhop 1.0  -off 0.  -u1 0.5  -u2 0.  -e 0.  -w 90  -zoom 15 -alpha 0.5  -norm 0.997  TOI1420_i_WST_43cm.tab
